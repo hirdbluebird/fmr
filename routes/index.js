300819116a17c9e11a2b06c05affb8c6f9bf792f
hirdbluebird <arthur.tkachenko.netweight@gmail.com>
2018-01-30 10:06:06 +0200

    api setup

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,4 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Music List' });
 });
 
-const testJSON = [
-	{
-		name: 'John Smith',
-		username: 'Captain Code'
-	},
-	{
-		name: 'Jane Doe',
-		username: 'JaneyCakes'
-	}
-];
-
-router.get('/sendjson', (req, res, next) => {
-	res.json(testJSON);
-});
-
 module.exports = router;
